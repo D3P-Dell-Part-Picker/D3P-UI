8dcc10829d7cd544ba85e3192315dd058aa4b859
Comeau <acomeau62203@gmail.com>
2019-11-08 12:20:08 -0500

    added a new cs file

---
+++ DellPartPicker/DataDump.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DellPartPicker
+{
+    class DataDump
+    {
+        Loader loader = new Loader();
+        Parser parser = new Parser();
+    }
+
+    class ParseList
+    {
+
+    }
+}
