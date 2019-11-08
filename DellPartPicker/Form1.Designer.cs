231c5b631acbfe1063ea1cde120535db22b1914d
Alex <longnamesrule@gmail.com>
2019-11-08 11:06:40 -0500

    tabs

--- DellPartPicker/Form1.Designer.cs
+++ DellPartPicker/Form1.Designer.cs
@@ -40,15 +40,30 @@
             this.searchBox = new System.Windows.Forms.TextBox();
             this.Searchlbl = new System.Windows.Forms.Label();
             this.pictureSettings = new System.Windows.Forms.PictureBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureSettings)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // IDsearch
             // 
             this.IDsearch.AutoSize = true;
-            this.IDsearch.Location = new System.Drawing.Point(244, 291);
+            this.IDsearch.Location = new System.Drawing.Point(320, 342);
             this.IDsearch.Name = "IDsearch";
             this.IDsearch.Size = new System.Drawing.Size(86, 17);
             this.IDsearch.TabIndex = 0;
@@ -59,7 +74,7 @@
             // nameSearch
             // 
             this.nameSearch.AutoSize = true;
-            this.nameSearch.Location = new System.Drawing.Point(244, 345);
+            this.nameSearch.Location = new System.Drawing.Point(320, 396);
             this.nameSearch.Name = "nameSearch";
             this.nameSearch.Size = new System.Drawing.Size(105, 17);
             this.nameSearch.TabIndex = 1;
@@ -70,7 +85,7 @@
             // locSearch
             // 
             this.locSearch.AutoSize = true;
-            this.locSearch.Location = new System.Drawing.Point(244, 396);
+            this.locSearch.Location = new System.Drawing.Point(320, 447);
             this.locSearch.Name = "locSearch";
             this.locSearch.Size = new System.Drawing.Size(118, 17);
             this.locSearch.TabIndex = 2;
@@ -81,18 +96,18 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::DellPartPicker.Properties.Resources.Dell_logo_2016;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 55);
+            this.pictureBox1.Location = new System.Drawing.Point(8, 87);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(151, 147);
+            this.pictureBox1.Size = new System.Drawing.Size(177, 133);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
             // 
             // search
             // 
-            this.search.Location = new System.Drawing.Point(12, 345);
+            this.search.Location = new System.Drawing.Point(36, 420);
             this.search.Name = "search";
-            this.search.Size = new System.Drawing.Size(217, 68);
+            this.search.Size = new System.Drawing.Size(110, 44);
             this.search.TabIndex = 4;
             this.search.Text = "Search";
             this.search.UseVisualStyleBackColor = true;
@@ -101,9 +116,9 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(368, 22);
+            this.dataGridView1.Location = new System.Drawing.Point(510, 19);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(552, 360);
+            this.dataGridView1.Size = new System.Drawing.Size(239, 346);
             this.dataGridView1.TabIndex = 5;
             // 
             // title
@@ -112,7 +127,7 @@
             this.title.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.title.Font = new System.Drawing.Font("Open Sans Condensed", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.title.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.title.Location = new System.Drawing.Point(4, 9);
+            this.title.Location = new System.Drawing.Point(6, 19);
             this.title.Name = "title";
             this.title.Size = new System.Drawing.Size(247, 43);
             this.title.TabIndex = 6;
@@ -122,7 +137,7 @@
             // 
             this.error.AutoSize = true;
             this.error.ForeColor = System.Drawing.Color.Maroon;
-            this.error.Location = new System.Drawing.Point(12, 329);
+            this.error.Location = new System.Drawing.Point(88, 380);
             this.error.Name = "error";
             this.error.Size = new System.Drawing.Size(76, 13);
             this.error.TabIndex = 7;
@@ -131,15 +146,15 @@
             // searchBox
             // 
             this.searchBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.searchBox.Location = new System.Drawing.Point(63, 290);
+            this.searchBox.Location = new System.Drawing.Point(139, 341);
             this.searchBox.Name = "searchBox";
-            this.searchBox.Size = new System.Drawing.Size(166, 20);
+            this.searchBox.Size = new System.Drawing.Size(114, 20);
             this.searchBox.TabIndex = 8;
             // 
             // Searchlbl
             // 
             this.Searchlbl.AutoSize = true;
-            this.Searchlbl.Location = new System.Drawing.Point(12, 291);
+            this.Searchlbl.Location = new System.Drawing.Point(88, 342);
             this.Searchlbl.Name = "Searchlbl";
             this.Searchlbl.Size = new System.Drawing.Size(44, 13);
             this.Searchlbl.TabIndex = 9;
@@ -148,38 +163,140 @@
             // pictureSettings
             // 
             this.pictureSettings.Image = ((System.Drawing.Image)(resources.GetObject("pictureSettings.Image")));
-            this.pictureSettings.Location = new System.Drawing.Point(872, 388);
+            this.pictureSettings.Location = new System.Drawing.Point(1172, 51);
             this.pictureSettings.Name = "pictureSettings";
-            this.pictureSettings.Size = new System.Drawing.Size(48, 50);
+            this.pictureSettings.Size = new System.Drawing.Size(10, 10);
             this.pictureSettings.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureSettings.TabIndex = 10;
             this.pictureSettings.TabStop = false;
             this.pictureSettings.Click += new System.EventHandler(this.pictureSettings_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(590, 40);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(257, 407);
+            this.dataGridView2.TabIndex = 11;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 288);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(266, 159);
+            this.textBox1.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 453);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 33);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(0, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(946, 530);
+            this.tabControl1.TabIndex = 14;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Controls.Add(this.pictureSettings);
+            this.tabPage1.Controls.Add(this.title);
+            this.tabPage1.Controls.Add(this.pictureBox1);
+            this.tabPage1.Controls.Add(this.search);
+            this.tabPage1.Controls.Add(this.searchBox);
+            this.tabPage1.Controls.Add(this.Searchlbl);
+            this.tabPage1.Controls.Add(this.IDsearch);
+            this.tabPage1.Controls.Add(this.nameSearch);
+            this.tabPage1.Controls.Add(this.error);
+            this.tabPage1.Controls.Add(this.locSearch);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(938, 504);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.pictureBox3);
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.pictureBox2);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.textBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(938, 504);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Open Sans Condensed", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Location = new System.Drawing.Point(5, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(247, 43);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Dell Part Picker UI";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::DellPartPicker.Properties.Resources.Dell_logo_2016;
+            this.pictureBox2.Location = new System.Drawing.Point(13, 70);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(224, 143);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 14;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(1194, 6);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(10, 10);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 16;
+            this.pictureBox3.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(932, 450);
-            this.Controls.Add(this.pictureSettings);
-            this.Controls.Add(this.Searchlbl);
-            this.Controls.Add(this.searchBox);
-            this.Controls.Add(this.error);
-            this.Controls.Add(this.title);
-            this.Controls.Add(this.dataGridView1);
-            this.Controls.Add(this.search);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.locSearch);
-            this.Controls.Add(this.nameSearch);
-            this.Controls.Add(this.IDsearch);
+            this.ClientSize = new System.Drawing.Size(942, 530);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureSettings)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -195,6 +312,15 @@
         public System.Windows.Forms.Label Searchlbl;
         public System.Windows.Forms.Label title;
         public System.Windows.Forms.TextBox searchBox;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        public System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
