daa00531fe8a37f1ee29ba666fb991fb491d325a
Alex <longnamesrule@gmail.com>
2019-11-26 08:39:56 -0500

    committing

---
+++ DellPartPicker/SocketComms.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace DellPartPicker
+{
+    class SocketComms
+    {
+        public static void sendMessage(String message, String ip, int port)
+        {
+
+            try
+            {
+
+                // Establish the remote endpoint  
+                // for the socket. This example  
+                // uses port 11111 on the local  
+                // computer. 
+                
+                IPAddress ipAddr = IPAddress.Parse(ip);
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
+
+                // Creation TCP/IP Socket using  
+                // Socket Class Costructor 
+                Socket sender = new Socket(ipAddr.AddressFamily,
+                           SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+
+                    // Connect Socket to the remote  
+                    // endpoint using method Connect() 
+                    sender.Connect(localEndPoint);
+
+                    // We print EndPoint information  
+                    // that we are connected 
+                    Console.WriteLine("Socket connected to -> {0} ",
+                                  sender.RemoteEndPoint.ToString());
+
+                    // Creation of messagge that 
+                    // we will send to Server 
+                    byte[] messageSent = Encoding.ASCII.GetBytes(message);
+                    int byteSent = sender.Send(messageSent);
+
+                    // Data buffer 
+
+                    // Close Socket using  
+                    // the method Close() 
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+
+                // Manage of Socket's Exceptions 
+                catch (ArgumentNullException ane)
+                {
+
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+
+                catch (SocketException se)
+                {
+
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
