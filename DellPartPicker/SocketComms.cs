9fd28ce879c50cd33f9c455f9eed695853cad1e6
Alex <longnamesrule@gmail.com>
2019-12-09 12:23:01 -0500

    ...

--- DellPartPicker/SocketComms.cs
+++ DellPartPicker/SocketComms.cs
@@ -1,78 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-<<<<<<< HEAD
-using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Sockets;
-=======
+
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
->>>>>>> dark_m/networking
+
 
 namespace DellPartPicker
 {
     class SocketComms
     {
-<<<<<<< HEAD
-        public static void sendMessage(String message, String ip, int port)
-        {
-
-            try
-            {
-
-                // Establish the remote endpoint  
-                // for the socket. This example  
-                // uses port 11111 on the local  
-                // computer. 
-                
-                IPAddress ipAddr = IPAddress.Parse(ip);
-                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, port);
-
-                // Creation TCP/IP Socket using  
-                // Socket Class Costructor 
-                Socket sender = new Socket(ipAddr.AddressFamily,
-                           SocketType.Stream, ProtocolType.Tcp);
-
-                try
-                {
-
-                    // Connect Socket to the remote  
-                    // endpoint using method Connect() 
-                    sender.Connect(localEndPoint);
-
-                    // We print EndPoint information  
-                    // that we are connected 
-                    Console.WriteLine("Socket connected to -> {0} ",
-                                  sender.RemoteEndPoint.ToString());
-
-                    // Creation of messagge that 
-                    // we will send to Server 
-                    byte[] messageSent = Encoding.ASCII.GetBytes(message);
-                    int byteSent = sender.Send(messageSent);
-
-                    // Data buffer 
-
-                    // Close Socket using  
-                    // the method Close() 
-                    sender.Shutdown(SocketShutdown.Both);
-                    sender.Close();
-                }
-
-                // Manage of Socket's Exceptions 
-                catch (ArgumentNullException ane)
-                {
-
-                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
-                }
-
-                catch (SocketException se)
-                {
-=======
 
         /*static void Main(string[] args)
         {
@@ -118,7 +58,6 @@ namespace DellPartPicker
                 }
 
                 catch(SocketException se) {
->>>>>>> dark_m/networking
 
                     Console.WriteLine("SocketException : {0}", se.ToString());
                 }
@@ -131,16 +70,9 @@ namespace DellPartPicker
 
             catch (Exception e)
             {
-<<<<<<< HEAD
 
                 Console.WriteLine(e.ToString());
             }
         }
-=======
-                Console.WriteLine(e.ToString());
-            }
-        }
-
->>>>>>> dark_m/networking
     }
 }
