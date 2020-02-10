162e03ae949a2924466fbf985c7243af1a2c62dc
alexm622 <longnamesrule@gmail.com>
2020-02-09 20:56:45 -0500

    moved some stuff around

--- DellPartPicker/Form1.Designer.cs
+++ DellPartPicker/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.map = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
             this.maperrorLabel = new System.Windows.Forms.Label();
             this.findbtn = new System.Windows.Forms.Button();
             this.bttnClear = new System.Windows.Forms.Button();
@@ -150,6 +151,7 @@
             // 
             // map
             // 
+            this.map.Controls.Add(this.button1);
             this.map.Controls.Add(this.maperrorLabel);
             this.map.Controls.Add(this.findbtn);
             this.map.Controls.Add(this.bttnClear);
@@ -216,6 +218,18 @@
             this.map.UseVisualStyleBackColor = true;
             this.map.Click += new System.EventHandler(this.map_Click);
             // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1261, 450);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(373, 78);
+            this.button1.TabIndex = 56;
+            this.button1.Text = "Next";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // maperrorLabel
             // 
             this.maperrorLabel.AutoSize = true;
@@ -232,12 +246,12 @@
             // 
             this.findbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.findbtn.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.findbtn.Location = new System.Drawing.Point(1256, 455);
+            this.findbtn.Location = new System.Drawing.Point(1261, 537);
             this.findbtn.Margin = new System.Windows.Forms.Padding(4);
             this.findbtn.Name = "findbtn";
-            this.findbtn.Size = new System.Drawing.Size(373, 154);
+            this.findbtn.Size = new System.Drawing.Size(373, 73);
             this.findbtn.TabIndex = 54;
-            this.findbtn.Text = "Find";
+            this.findbtn.Text = "Previous";
             this.findbtn.UseVisualStyleBackColor = true;
             this.findbtn.Click += new System.EventHandler(this.findbtn_Click);
             // 
@@ -1386,6 +1400,7 @@
         protected internal System.Windows.Forms.TabControl tabControl;
         public System.Windows.Forms.Button findbtn;
         private System.Windows.Forms.Label maperrorLabel;
+        public System.Windows.Forms.Button button1;
     }
 }
 
